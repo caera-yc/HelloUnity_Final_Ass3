3ef29435061c9eac3acdc79c08f4904e2ea02fc6
caera-yc <38510526+caera-yc@users.noreply.github.com>
2018-05-30 09:43:24 +1200

    Initial commit

---
+++ Assets/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Assignment
+{
+    //List all the columns of your table here in the same format as below.
+    public string ListName { get; set; }
+    public string CardName { get; set; }
+    public string CardURL { get; set; }
+    public string CreatorFullName { get; set; }
+}
